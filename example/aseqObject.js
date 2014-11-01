220ed7f2dd7ba38ef614622f71958da297225868
maximsmol <maximsmol@aristotle.local>
2014-11-01 14:09:05 +0300

    Updated example

--- example/aseqObject.js
+++ example/aseqObject.js
@@ -4,32 +4,32 @@ var aseq = require('../lib/aseq');
 
 var a = function(a, b, c, callback)
 {
-	console.log(arguments);
+	console.log(arguments, this);
 	callback('callback argument');
 };
 
-var b = function(a, b, c, callback)
+var b = function(a, callback)
 {
-	console.log(arguments);
+	console.log(arguments, this);
 	callback('callback argument');
 };
 
 var c = function(a, b, c, callback)
 {
-	console.log(arguments);
+	console.log(arguments, this);
 	callback('callback argument');
 };
 
-var d = function(a, b, c, callback)
+var d = function(a, callback)
 {
-	console.log(arguments);
+	console.log(arguments, this);
 	callback('callback argument');
 };
 
 var seq = new aseq();
 seq.append(a, null, 'a', 'b');
-seq.append(b, null, 'c', 'd');
-seq.append(c, null, 'e', 'f');
-seq.append(d, null, 'g', 'h');
+seq.append(b, {test: 'test'});
+seq.append(c, {test: 'test'}, 'e', 'f');
+seq.append(d);
 
 seq.run('additional argument');
